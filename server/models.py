653b030f6a0857f5fcea6e16631c3caba229d6d2
TradingCyborg <my.github.kinyuad29@gmail.com>
2024-01-24 20:41:02 +0300

    commits

--- server/models.py
+++ server/models.py
@@ -4,9 +4,9 @@ from sqlalchemy.orm import validates
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
 
-# metadata = MetaData(naming_convention={
-#     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-# })
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+ })
 
 db = SQLAlchemy()
 
